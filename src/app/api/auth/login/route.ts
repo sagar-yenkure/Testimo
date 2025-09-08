fe4a81d3fbb3d350ea0b8891815f653c0a5de33e
sagaryenkure <sagaryenkure@vionsys.com>
2025-09-08 12:52:01 +0530

    Add JWT authentication and rate limiting to login endpoint.

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,61 @@
+import { ratelimit } from "@/trpc/middleware/rateLimitor";
+import authService from "@/trpc/services/auth.service";
+import { generateToken } from "@/utils/generateToken";
+import { getClientIP } from "@/utils/getClientIP";
+import { NextResponse } from "next/server";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json();
+
+    const { success } = await ratelimit.limit(getClientIP(req));
+    if (!success)
+      return NextResponse.json(
+        {
+          message: "Too many attempts. Please try again later.",
+        },
+        { status: 429, headers: corsHeaders }
+      );
+
+    const user = await authService.isUserExist(email);
+
+    if (!user)
+      return NextResponse.json(
+        {
+          message: "Invalid Credentials, Try Again",
+        },
+        { status: 429, headers: corsHeaders }
+      );
+
+    const isSame = authService.comparePassword(password, user?.password || "");
+
+    if (!isSame)
+      return NextResponse.json(
+        {
+          message: "Invalid Credentials, Try Again",
+        },
+        { status: 429, headers: corsHeaders }
+      );
+
+    const token = generateToken({
+      id: user.id,
+      email: user.email,
+      plan: user.plan,
+      username: user.name,
+    });
+
+    return NextResponse.json({ token }, { status: 200, headers: corsHeaders });
+  } catch (error) {
+    console.error("Error occurred during login:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500, headers: corsHeaders }
+    );
+  }
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,9 @@
 export default {
-    AWS_KEYS: {
-        region: process.env.AMAZON_REGION,
-        accessKeyId: process.env.AMAZON_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AMAZON_SECRET_ACCESS_KEY,
-        bucket: process.env.AMAZON_BUCKET_NAME,
-    },
-
-    REDIS_URL: process.env.REDIS_URL,
-}
+  AWS_KEYS: {
+    region: process.env.AMAZON_REGION,
+    accessKeyId: process.env.AMAZON_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AMAZON_SECRET_ACCESS_KEY,
+    bucket: process.env.AMAZON_BUCKET_NAME,
+  },
+  JWT_SECRET: process.env.JWT_SECRET,
+};
